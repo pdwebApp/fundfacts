234243d0d91101696f7e90ca427775df1cd25c75
pdwebApp <67463874+pdwebApp@users.noreply.github.com>
2023-08-30 12:20:22 +0000

    added some dropdown menus

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask import render_template as rt
 app = Flask(__name__)
 @app.route('/')
 def helloworld():
-  return "Hello World"
+  return rt('home.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug = True)
